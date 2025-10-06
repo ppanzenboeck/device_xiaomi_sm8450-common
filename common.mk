6faa0d204959101af25d3c91b006c539063efec0
bomboozler <yarinqwq121@gmail.com>
2025-10-06 16:51:50 +0300

    switched build tag to offical

--- common.mk
+++ common.mk
@@ -498,7 +498,7 @@ PRODUCT_PACKAGES += \
     firmware_qca6750_wlan_mac.bin_symlink
 
 # Derpfest build type
-DERPFEST_BUILD_TYPE := Community
+DERPFEST_BUILD_TYPE := Offical
 
 # Remove Packages
 PRODUCT_PACKAGES += \
