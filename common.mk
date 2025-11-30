07049fb91056ba2d981ebbd6509a343dcdc541dc
bomboozler <yarinqwq121@gmail.com>
2025-11-30 19:25:58 +0200

    include GameBar if-exists
    Change-Id: I19ac63ca1e30059a2021d89b855eb0c645689e73

--- common.mk
+++ common.mk
@@ -518,3 +518,6 @@ $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 # Overlays-derp
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
+
+# GameBar Performance Overlay
+$(call inherit-product-if-exists, packages/apps/GameBar/gamebar.mk)
